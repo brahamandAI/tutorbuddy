c4d2e2d7243aa86b4b9387df5b563e6c04fef77b
Ubuntu <ubuntu@srv812233.hstgr.cloud>
2025-11-24 13:48:09 +0000

    eco\sys

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,9 @@ module.exports = {
       autorestart: false,
       watch: false,
       max_memory_restart: '1G',
+      max_restarts: 5,
+      restart_delay: 5000,       // 5 seconds delay
+      exp_backoff_restart_delay: 200, // exponential backoff
       stop_exit_codes: '0',
       env: {
         NODE_ENV: 'production',
